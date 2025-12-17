0d3f7f295f94d8d54b468d66b64fba09e1c0a663
kuaile-dayday <75664563+kuaile-dayday@users.noreply.github.com>
2025-12-17 17:26:33 +0800

    day7 缓存菜品 引入了redis 这里相关问题“如何保持redis和数据库的一致性”

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -12,9 +12,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /*
 * 菜品管理
@@ -28,6 +30,9 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     /*
     * 新增菜品和对应的口味
     * */
@@ -36,6 +41,12 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品：{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        // 清理缓存数据
+        String key = "dish_" + dishDTO.getCategoryId();
+        cleanCache(key);
+
+
         return Result.success();
 
     }
@@ -60,6 +71,11 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("批量删除菜品，ids：{}",ids);
         dishService.delete(ids);
+
+        // 清理缓存数据 因为这里的ids可能是一个也可能是多个，如果要去判断删除对应的就很麻烦，
+        //　所以这里直接将所有的菜品缓存数据都清理掉，所有以ｄｉｓｈ＿开头的ｋｅｙ
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -82,6 +98,12 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品：{}",dishDTO);
         dishService.update(dishDTO);
+
+        // 清理缓存数据 因为这里的ids可能是一个也可能是多个，如果要去判断删除对应的就很麻烦，
+        //　所以这里直接将所有的菜品缓存数据都清理掉，所有以ｄｉｓｈ＿开头的ｋｅｙ
+        cleanCache("dish_*");
+
+
         return Result.success();
     }
 
@@ -105,7 +127,20 @@ public class DishController {
     public Result startOrStop(@PathVariable Integer status, Long id){
         log.info("启用或禁用菜品：{}",id);
         dishService.startOrStop(status,id);
+
+        // 清理缓存数据 因为这里的ids可能是一个也可能是多个，如果要去判断删除对应的就很麻烦，
+        //　所以这里直接将所有的菜品缓存数据都清理掉，所有以ｄｉｓｈ＿开头的ｋｅｙ
+        cleanCache("dish_*");
+
         return Result.success();
+    }
+
+    /*
+    * 清理缓存数据
+    * */
+    private void cleanCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 
 
