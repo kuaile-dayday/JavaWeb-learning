cd6b9d1f0d69aec0fef63c440d722b8b59015e6d
kuaile-dayday <75664563+kuaile-dayday@users.noreply.github.com>
2025-12-23 19:42:52 +0800

    day10 订单状态定时处理 引入Spring Task cron表达式

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,8 @@ import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.core.annotation.Order;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -47,4 +49,10 @@ public interface OrderMapper {
     * */
     @Select("select count(id) from orders where status = #{status}")
     Integer countStatus(Integer toBeConfirmed);
+
+    /*
+    * 根据订单和状态查询时间是否超时
+    * */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
