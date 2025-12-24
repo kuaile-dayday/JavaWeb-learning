a759c8c6ba1b3148ecc95d81f1d056418da5e105
kuaile-dayday <75664563+kuaile-dayday@users.noreply.github.com>
2025-12-24 17:13:07 +0800

    day11 数据可视化 图表统计那些 引入ECharts

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -55,4 +55,23 @@ public interface OrderMapper {
     * */
     @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
     List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
+    /**
+     * 根据动态条件统计营业额
+     * @param map
+     */
+    Double sumByMap(Map map);
+
+    /**
+     *根据动态条件统计订单数量
+     * @param map
+     */
+    Integer countByMap(Map map);
+
+    /**
+     * 查询商品销量排名
+     * @param begin
+     * @param end
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
