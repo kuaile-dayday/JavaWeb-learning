182594b16c76cb76d3fcf0dbf3ad1eb6ca2e9b43
kuaile-dayday <75664563+kuaile-dayday@users.noreply.github.com>
2025-12-26 17:11:43 +0800

    day12 数据导出 引入POI 可以对Excel文件读写

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -59,4 +60,11 @@ public interface DishMapper {
     * 动态条件查询菜品
     * */
     List<Dish> list(Dish dish);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
